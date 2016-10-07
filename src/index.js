7a42cdaaabdd2451b1ec2989dab9b083fb542e9e
Eric Eslinger <eric.eslinger@gmail.com>
2016-10-07 15:25:46 -0700

    exporting stuff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import {Guild} from './guild';
 import {Model} from './model';
+import {SQLStorage} from './storage/sql';
+import {RedisStorage} from './storage/sql';
+import {RestStorage} from './storage/sql';
+import {LocalForageStorage} from './storage/sql';
+import {MemoryStorage} from './storage/sql';
 
-export {Guild, Model};
+export {
+  Guild,
+  Model,
+  SQLStorage,
+  RedisStorage,
+  RestStorage,
+  LocalForageStorage,
+  MemoryStorage,
+};
