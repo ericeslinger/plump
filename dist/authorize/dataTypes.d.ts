457a199e69f45a3313bb3722a71c9a030e8c263b
Eric Eslinger <eric.eslinger@gmail.com>
2017-04-28 15:46:09 -0700

    chore: build

--- dist/authorize/dataTypes.d.ts
+++ dist/authorize/dataTypes.d.ts
@@ -1,34 +1,89 @@
 import { ModelReference, IndefiniteModelData, ModelData } from '../dataTypes';
-export interface AttributesAuthorize {
-    authorizeCreate(actor: ModelReference, item: ModelReference, data: IndefiniteModelData): any;
-    authorizeRead(actor: ModelReference, item: ModelReference): any;
-    authorizeUpdate(actor: ModelReference, item: ModelReference, data: ModelData): any;
-    authorizeDelete(actor: ModelReference, item: ModelReference): any;
+export interface AbstractAuthorizeRequest {
+    kind: 'attributes' | 'relationship' | 'compound';
 }
-export interface RelationshipAuthorizeArguments {
+export interface AbstractAttributesAuthorizeRequest extends AbstractAuthorizeRequest {
+    action: 'create' | 'read' | 'update' | 'delete';
+    kind: 'attributes';
     actor: ModelReference;
-    parent: ModelReference;
-    relationship: string;
-    child: ModelReference;
-    meta?: any;
+}
+export interface AttributesReadAuthorizeRequest extends AbstractAttributesAuthorizeRequest {
+    action: 'read';
+    target: ModelReference;
+}
+export interface AttributesDeleteAuthorizeRequest extends AbstractAttributesAuthorizeRequest {
+    action: 'delete';
+    target: ModelReference;
+}
+export interface AttributesCreateAuthorizeRequest extends AbstractAttributesAuthorizeRequest {
+    action: 'create';
     data?: IndefiniteModelData;
 }
+export interface AttributesUpdateAuthorizeRequest extends AbstractAttributesAuthorizeRequest {
+    action: 'update';
+    target: ModelReference;
+    data?: ModelData;
+}
+export declare type AttributesAuthorizeRequest = AttributesCreateAuthorizeRequest | AttributesReadAuthorizeRequest | AttributesUpdateAuthorizeRequest | AttributesDeleteAuthorizeRequest;
+export interface AbstractRelationshipAuthorizeRequest extends AbstractAuthorizeRequest {
+    kind: 'relationship';
+    action: 'create' | 'read' | 'update' | 'delete';
+    actor: ModelData;
+    relationship: string;
+    parent: ModelReference;
+}
+export interface RelationshipCreateAuthorizeRequest extends AbstractRelationshipAuthorizeRequest {
+    action: 'create';
+    child: ModelReference;
+    meta?: any;
+}
+export interface RelationshipReadAuthorizeRequest extends AbstractRelationshipAuthorizeRequest {
+    action: 'read';
+}
+export interface RelationshipUpdateAuthorizeRequest extends AbstractRelationshipAuthorizeRequest {
+    action: 'update';
+    child: ModelReference;
+    meta?: any;
+}
+export interface RelationshipDeleteAuthorizeRequest extends AbstractRelationshipAuthorizeRequest {
+    action: 'delete';
+    child: ModelReference;
+}
+export declare type RelationshipAuthorizeRequest = RelationshipCreateAuthorizeRequest | RelationshipReadAuthorizeRequest | RelationshipUpdateAuthorizeRequest | RelationshipDeleteAuthorizeRequest;
+export declare type SimpleAuthorizeRequest = RelationshipAuthorizeRequest | AttributesAuthorizeRequest;
+export interface CompoundAuthorizeRequest extends AbstractAuthorizeRequest {
+    kind: 'compound';
+    combinator: 'and' | 'or';
+    list: (AttributesAuthorizeRequest | RelationshipAuthorizeRequest | CompoundAuthorizeRequest)[];
+}
+export declare type AuthorizeRequest = RelationshipAuthorizeRequest | AttributesAuthorizeRequest | CompoundAuthorizeRequest;
+export interface AbstractAuthorizeResponse {
+    kind: string;
+}
+export interface FinalAuthorizeResponse extends AbstractAuthorizeResponse {
+    kind: 'final';
+    result: boolean;
+}
+export interface DelegateAuthorizeResponse extends AbstractAuthorizeResponse {
+    kind: 'delegated';
+    delegate: AuthorizeRequest;
+}
+export declare type AuthorizeResponse = FinalAuthorizeResponse | DelegateAuthorizeResponse;
+export interface AttributesAuthorize {
+    authorizeCreate(AttributesCreateAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeRead(AttributesReadAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeUpdate(AttributesUpdateAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeDelete(AttributesDeleteAuthorizeRequest: any): Promise<AuthorizeResponse>;
+}
 export interface RelationshipAuthorize {
-    authorizeCreate(actor: ModelReference, opts: RelationshipAuthorizeArguments): any;
-    authorizeRead(actor: ModelReference, opts: RelationshipAuthorizeArguments): any;
-    authorizeUpdate(actor: ModelReference, opts: RelationshipAuthorizeArguments): any;
-    authorizeDelete(actor: ModelReference, opts: RelationshipAuthorizeArguments): any;
+    authorizeCreate(RelationshipCreateAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeRead(RelationshipReadAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeUpdate(RelationshipUpdateAuthorizeRequest: any): Promise<AuthorizeResponse>;
+    authorizeDelete(RelationshipDeleteAuthorizeRequest: any): Promise<AuthorizeResponse>;
 }
 export interface AuthorizerDefinition {
-    typeName: string;
     attributes: AttributesAuthorize;
     relationships: {
         [name: string]: RelationshipAuthorize;
     };
-}
-export interface AuthorizeReturn {
-    or?: AuthorizeReturn[];
-    and?: AuthorizeReturn[];
-    actual?: boolean;
-    final: boolean;
 }
